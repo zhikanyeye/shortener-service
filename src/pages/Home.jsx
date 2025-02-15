622b671f766571fc1fe41af5335b53e2e97509d6
Luxcus Qing <ai2845612055@outlook.com>
2025-02-16 00:37:55 +0800

    Create Home.jsx

---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import CreateForm from '../CreateForm';
+
+function Home() {
+  return (
+    <div className="home">
+      <h1>URL Shortener</h1>
+      <CreateForm />
+    </div>
+  );
+}
+
+export default Home;
